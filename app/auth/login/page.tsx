3c880f7a059c60cabab40c9f8224b7cdfaa94928
gimbal-com <senseofpriderenovation@gmail.com>
2025-02-09 19:15:13 -0800

    add:ui:login and register component add

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,32 +1,104 @@
-import { FC } from "react";
+'use client';
+
+import { FC } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
 import {
     Card,
     CardContent,
     CardHeader,
     CardFooter,
     CardTitle
-} from "@/components/ui/card";
-import Image from "next/image";
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form';
 
-import Logo from "../../../assets/images/logo.png";
-import { Input } from "@/components/ui/input";
+import Image from 'next/image';
+import Logo from '../../../assets/images/logo.png';
+import Link from 'next/link';
+
+const formSchema = z.object({
+    username: z.string().min(1, { message: 'Username is required.' }),
+    password: z.string().min(1, { message: 'Password is required.' })
+})
 
 const LoginPage: FC = () => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: '',
+            password: ''
+        }
+    })
+
+    const handleSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
     return (
-        <Card className="w-96">
+        <Card className='w-96'>
             <CardHeader>
-                <CardTitle>Login</CardTitle>
-                <Image src={Logo} width={200} height={100} alt="Logo" />
+                <CardTitle className='text-center mb-4'>
+                    <h2 className='text-xl'>
+                        Login
+                    </h2>
+                </CardTitle>
+                <Image src={Logo} width={400} height={200} alt='Logo' />
             </CardHeader>
             <CardContent>
-                <div className="flex flex-col gap-3">
-                    <Input placeholder="Username" />
-                    <Input placeholder="Password" />
-                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-2'>
+                        <FormField
+                            control={form.control}
+                            name='username'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder='Username' {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='password'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder='Password' type='password' {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type='submit' className='w-full my-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                            </svg>
+                            Login
+                        </Button>
+                    </form>
+                </Form>
+                <p className='text-sm my-3'>
+                    Don't you have an account?
+                    <Link href={'/auth/register'} className='text-blue-600 hover:underline ml-2'>Create One</Link>
+                </p>
             </CardContent>
-            <CardFooter>
-
-            </CardFooter>
         </Card>
     )
 }
---
+++ app/auth/register/page.tsx
@@ -0,0 +1,148 @@
+'use client';
+
+import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form';
+import { 
+    Select, 
+    SelectContent, 
+    SelectItem, 
+    SelectTrigger,
+    SelectValue
+} from '@/components/ui/select';
+
+import Logo from '../../../assets/images/logo.png';
+
+const formSchema = z.object({
+    username: z.string().min(1, {message: 'Username is required.'}),
+    password: z.string().min(1, {message: 'Password is required.'}),
+    email: z.string().email(),
+    type: z.enum(['client', 'pilot'])
+})
+
+const LoginPage: FC = () => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: '',
+            password: '',
+            email: '',
+            type: 'client'
+        }
+    })
+
+    const handleSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
+    return (
+        <Card className='w-96'>
+            <CardHeader>
+                <CardTitle className='text-center mb-4'>
+                    <h2 className='text-xl'>
+                        Register
+                    </h2>
+                </CardTitle>
+                <Image src={Logo} width={400} height={200} alt='Logo' />
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-2'>
+                        <FormField
+                            control={form.control}
+                            name='username'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder='Username' {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='password'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder='Password' type='password' {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='email'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder='Email' {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='type'
+                            render={({ field}) => (
+                                <FormItem>
+                                    <FormLabel>Account Type</FormLabel>
+                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder='Select account type' />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value='client'>Client</SelectItem>
+                                            <SelectItem value='pilot'>Pilot</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type='submit' className='w-full my-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                            </svg>
+                            Register
+                        </Button>
+                    </form>
+                </Form>
+                <p className='text-sm my-3'>
+                    Already have an account?
+                    <Link href={'/auth/login'} className='text-blue-600 hover:underline ml-2'>Login</Link>
+                </p>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default LoginPage;
